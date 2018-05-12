f9379ccdd1d871114219ba92f23af66135e62823
Bart Massey <bart@cs.pdx.edu>
2018-05-12 01:41:06 -0700

    cleaned up and rustdoced

--- shuffle.rs
+++ shuffle.rs
@@ -1,29 +1,25 @@
+// Copyright © 2018 Bart Massey
+
+//! Iterator that produces the items of a `Vec` in shuffled
+//! order.
+
 extern crate rand;
 use rand::Rng;
 
-/// Iterator that produces the items of a `Vec` in shuffled
-/// order.
-struct Shuffle<'a, T: 'a> {
+/// Shuffle iterator state.
+pub struct ShuffleIter<'a, T: 'a> {
     vec: &'a Vec<T>,
     posn: Vec<usize>,
     index: usize,
 }
 
-trait Shuffled<T> {
-    fn iter_shuffle(&self) -> Shuffle<T>;
-}
+impl <'a, T> ShuffleIter<'a, T> {
 
-impl <T> Shuffled<T> for Vec<T> {
-    fn iter_shuffle(&self) -> Shuffle<T> {
-        Shuffle::new(&self)
-    }
-}
-
-impl <'a, T> Shuffle<'a, T> {
-    fn new(vec: &Vec<T>) -> Shuffle<T> {
+    /// Create a new shuffle iterator instance.
+    fn new(vec: &Vec<T>) -> ShuffleIter<T> {
         let mut posn: Vec<usize> = (0..vec.len()).collect();
         rand::thread_rng().shuffle(&mut posn);
-        Shuffle {
+        ShuffleIter {
             vec,
             posn,
             index: 0,
@@ -31,7 +27,7 @@ impl <'a, T> Shuffle<'a, T> {
     }
 }
 
-impl <'a, T> Iterator for Shuffle<'a, T> {
+impl <'a, T> Iterator for ShuffleIter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         if self.index >= self.posn.len() {
@@ -43,10 +39,29 @@ impl <'a, T> Iterator for Shuffle<'a, T> {
     }
 }
 
-fn main() {
-    let vec: Vec<usize> = (1..6).collect();
-    for val in vec.iter_shuffle() {
-        println!("{}", val);
+/// Utility trait for being able to say
+/// *collection*`.iter_shuffle()`.
+pub trait Shuffle<T> {
+    /// Return references to the elements of a collection in
+    /// shuffled order, without disturbing the collection.
+    fn iter_shuffle(&self) -> ShuffleIter<T>;
+}
+
+impl <T> Shuffle<T> for Vec<T> {
+    fn iter_shuffle(&self) -> ShuffleIter<T> {
+        ShuffleIter::new(&self)
     }
-    println!("{:?}", vec);
+}
+
+// This test cheats by inspecting the iterator state,
+// because otherwise testing becomes hard and expensive.
+#[test]
+fn basic_test() {
+    let vec: Vec<usize> = (1..6).collect();
+    let shuffle = ShuffleIter::new(&vec);
+    assert_eq!(vec, (1..6).collect::<Vec<usize>>());
+    let posn = shuffle.posn.clone();
+    for (index, val) in shuffle.enumerate() {
+        assert_eq!(*val, vec[posn[index]]);
+    }
 }
